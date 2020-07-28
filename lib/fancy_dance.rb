6ac8cf1aaa51d61a2dadb89644144e055404487e
William Han Findiesen <william.findiesen@gmail.com>
2020-07-28 04:08:08 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -16,7 +16,8 @@ module FancyDance
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
-    
+  end
+  
     module ClassMethods
       
       def metadata
