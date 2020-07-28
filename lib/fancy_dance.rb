779a0da07a3fad2f8914079a9909bd95a2d7d87a
William Han Findiesen <william.findiesen@gmail.com>
2020-07-28 03:53:07 +0000

    Automatically backed up by Learn

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,15 @@
+module FancyDance
+  module InstanceMethods
+    
+    def twirl
+      "I'm twirling!"
+    end
+    
+    def jump
+      "Look how high I'm jumping!"
+    end
+    
+    def pirouette
+      
+    
+end
