a45ac303569e46ad76fdc30c5927ae45ed8dd4d0
William Han Findiesen <william.findiesen@gmail.com>
2020-07-28 03:55:23 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,6 +4,9 @@ require_relative './dance_module.rb'
 class Dancer
   include Dance
   extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  
   attr_accessor :name
   def initialize(name)
     @name = name
