509ff901657f59502b852c3a3a9dfa1488ff35a9
William Han Findiesen <william.findiesen@gmail.com>
2020-07-28 04:01:49 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,5 +1,5 @@
-require_relative './fancy_dance.rb'
-require_relative './dance_module.rb'
+require_relative './.rb'
+require_relative './class_methods_module.rb'
 
 module FancyDance
   module InstanceMethods
